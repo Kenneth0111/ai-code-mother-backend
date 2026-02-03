56fe95a4641ed1feb6568ffdb2044251d4d07bab
Kenneth0111 <694818974@qq.com>
2026-02-03 11:49:16 +0800

    AI应用生成 + SSE流式输出

---
+++ src/main/java/com/example/aicodemother/ai/AiCodeGeneratorServiceFactory.java
@@ -0,0 +1,43 @@
+package com.example.aicodemother.ai;
+
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.StreamingChatModel;
+import dev.langchain4j.service.AiServices;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * AI服务创建工厂
+ */
+@Configuration
+public class AiCodeGeneratorServiceFactory {
+
+    @Resource
+    private ChatModel chatModel;
+
+    @Resource
+    private StreamingChatModel streamingChatModel;
+
+    /**
+     * 创建AI代码生成器服务
+     * @return
+     */
+/*    @Bean
+    public AiCodeGeneratorService aiCodeGeneratorService() {
+        return AiServices.create(AiCodeGeneratorService.class, chatModel);
+    }*/
+
+    /**
+     * 创建AI代码生成器服务-流式输出
+     * @return
+     */
+    @Bean
+    public AiCodeGeneratorService aiCodeGeneratorService() {
+        return AiServices.builder(AiCodeGeneratorService.class)
+                .chatModel(chatModel)
+                .streamingChatModel(streamingChatModel)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/example/aicodemother/core/CodeFileSaver.java
@@ -0,0 +1,72 @@
+package com.example.aicodemother.core;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.StrUtil;
+import com.example.aicodemother.ai.model.HtmlCodeResult;
+import com.example.aicodemother.ai.model.MultiFileCodeResult;
+import com.example.aicodemother.model.enums.CodeGenTypeEnum;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 文件保存器
+ */
+public class CodeFileSaver {
+
+    /**
+     * 文件保存的根目录
+     */
+    private static final String FILE_SAVE_ROOT_DIR = System.getProperty("user.dir") + "/tmp/code_output";
+
+    /**
+     * 保存HTML网页代码
+     *
+     * @param htmlCodeResult
+     * @return
+     */
+    public static File saveHtmlCodeResult(HtmlCodeResult htmlCodeResult) {
+        String baseDirPath = buildUniqueDir(CodeGenTypeEnum.HTML.getValue());
+        writeToFile(baseDirPath, "index.html", htmlCodeResult.getHtmlCode());
+        return new File(baseDirPath);
+    }
+
+    /**
+     * 保存多文件代码
+     *
+     * @param multiFileCodeResult
+     * @return
+     */
+    public static File saveMultiFileCodeResult(MultiFileCodeResult multiFileCodeResult) {
+        String baseDirPath = buildUniqueDir(CodeGenTypeEnum.MULTI_FILE.getValue());
+        writeToFile(baseDirPath, "index.html", multiFileCodeResult.getHtmlCode());
+        writeToFile(baseDirPath, "style.scc", multiFileCodeResult.getCssCode());
+        writeToFile(baseDirPath, "script.js", multiFileCodeResult.getJsCode());
+        return new File(baseDirPath);
+    }
+
+    /**
+     * 构建文件的唯一路径：tem/code_output/bizType_雪花ID
+     *
+     * @param bizType
+     */
+    private static String buildUniqueDir(String bizType) {
+        String uniqueDirName = StrUtil.format("{}_{}", bizType, IdUtil.getSnowflakeNextIdStr());
+        String dirPath = FILE_SAVE_ROOT_DIR + File.separator + uniqueDirName;
+        FileUtil.mkdir(dirPath);
+        return dirPath;
+    }
+
+    /**
+     * 保存单个文件
+     *
+     * @param dirPath
+     * @param filename
+     * @param content
+     */
+    private static void writeToFile(String dirPath, String filename, String content) {
+        String filePath = dirPath + File.separator + filename;
+        FileUtil.writeString(content, filePath, StandardCharsets.UTF_8);
+    }
+}

38315e5f1db26af04455bd1b8f84c78357a9c40d
Kenneth0111 <694818974@qq.com>
2026-03-29 22:38:29 +0800

    第 14 期 - 微服务改造 Dubbo + Nacos 跨服务调用

--- ai-code-mother-microservice/ai-code-app/src/main/java/com/example/aicodemother/AiCodeAppApplication.java
+++ ai-code-mother-microservice/ai-code-app/src/main/java/com/example/aicodemother/AiCodeAppApplication.java
@@ -1,6 +1,7 @@
 package com.example.aicodemother;
 
 import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStoreAutoConfiguration;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,10 +10,11 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 @SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
 @MapperScan("com.example.aicodemother.mapper")
-public class AiCodeMotherApplication {
+@EnableDubbo
+public class AiCodeAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AiCodeMotherApplication.class, args);
+        SpringApplication.run(AiCodeAppApplication.class, args);
     }
 
 }
